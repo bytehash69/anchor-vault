4effe68bc91bc44ad99fdfd4cff1248f166181a1
Adiflickzzz <adivardhan2248@gmail.com>
2025-07-13 01:50:58 +0530

    Added documentation notes

--- programs/blueshift_anchor_vault/src/lib.rs
+++ programs/blueshift_anchor_vault/src/lib.rs
@@ -8,48 +8,70 @@ pub mod blueshift_anchor_vault {
 
     use super::*;
 
-    pub fn deposit(ctx: Context<VaultAction>, amount: u64) -> Result<()> {
-        require_eq!(ctx.accounts.vault.lamports(), 0,VaultError::VaultAlreadyExists);
-        require_gt!(amount, Rent::get()?.minimum_balance(0));
 
+    pub fn deposit(ctx:Context<VaultAction>, amount: u64) -> Result<()> {
+
+        // Checks if the vault account alreadly exists
+        require_eq!(ctx.accounts.vault.lamports(), 0, VaultError::VaultAlreadyExists);
+        // Checks if the amount being depositing is greater than the minimum balance require to create the vault account
+        require_gt!(amount, Rent::get()?.minimum_balance(0), VaultError::InvalidAmount);
+        
+        // Getting all the accounts required
+        let signer = ctx.accounts.signer.to_account_info();
+        let vault = ctx.accounts.vault.to_account_info();
+        let system_program = ctx.accounts.system_program.to_account_info();
+        
+        // Creating the cpi context required for the transfer function 
         let cpi_context = CpiContext::new(
-            ctx.accounts.system_program.to_account_info(),
-            Transfer {
-                from: ctx.accounts.signer.to_account_info(),
-                to:ctx.accounts.vault.to_account_info()
-            }
+            system_program,
+            // Transfer amount from signer to vault 
+            Transfer{from: signer,to: vault}
         );
+        
+        // CPI into the system program for the transfer function
         transfer(cpi_context, amount)?;
+        
         Ok(())
     }
+    
+    pub fn withdraw(ctx:Context<VaultAction>, amount: u64) -> Result<()> {
 
-    pub fn withdraw(ctx: Context<VaultAction>,amount: u64) -> Result<()> {
-        require_neq!(ctx.accounts.vault.lamports(), 0,VaultError::VaultAlreadyExists);
+        // Checks if there is some amount in the vault
+        require_neq!(ctx.accounts.vault.lamports(), 0, VaultError::InvalidAmount);
 
-
-        let signer_key = ctx.accounts.signer.key();
-        let signer_seeds: &[&[u8]] = &[b"vault", signer_key.as_ref(), &[ctx.bumps.vault]];
+        // Getting all the accounts required
+        let signer = ctx.accounts.signer.to_account_info();
+        let vault = ctx.accounts.vault.to_account_info();
+        let system_program = ctx.accounts.system_program.to_account_info();
+        
+        // Creating the seeds for vault pda
+        let signer_key = signer.key();
+        let signer_seeds: &[&[u8]] = &[b"byte",signer_key.as_ref(),&[ctx.bumps.vault]];
         let seeds = &[signer_seeds];
 
+        // Creating the cpi context required for the transfer function 
+        // and passing the seeds to derive the vault
         let cpi_context = CpiContext::new_with_signer(
-            ctx.accounts.system_program.to_account_info(),
-            Transfer {
-                from: ctx.accounts.vault.to_account_info(),
-                to: ctx.accounts.signer.to_account_info()
-            },
+            system_program,
+            // Transfer amount from vault to signer 
+            Transfer{from: vault,to: signer},
+            // The seeds
             seeds
         );
 
         transfer(cpi_context, amount)?;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
 pub struct VaultAction<'info> {
+    // The signer who wants to deposit and pay for the transactions
     #[account(mut)]
     pub signer: Signer<'info>,
 
+    // A system account pda for vault
     #[account(
         mut,
         seeds = [b"vault",signer.key().as_ref()],
@@ -57,6 +79,7 @@ pub struct VaultAction<'info> {
     )]
     pub vault: SystemAccount<'info>,
 
+    // System program for creating the pda
     pub system_program: Program<'info,System>
 }
 
